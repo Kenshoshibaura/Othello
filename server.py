7ea89425f63554528e24fe487e45765ceeddac22
Kenshoshibaura <50857122+Kenshoshibaura@users.noreply.github.com>
2020-03-04 21:17:21 +0900

    serverを更新

--- server.py
+++ server.py
@@ -7,21 +7,21 @@ port_num = 17086
 def get_html():
     return render_template('./index.html')
 
-@app.route('/lux', methods=['POST'])
+@app.route('/select', methods=['POST'])
 def update_lux():
-    time=request.form["time"]
-    lux=request.form["lux"]
-    #select=request.form["lux"]
+    #time=request.form["time"]
+    #lux=request.form["lux"]
+    select=request.form["select"]
     try:
         f = open(file_path,'w')
-        f.write(time+","+lux)
+        f.write(select)
         return "succeeded to write"
     except Exception as e:
         print(e)
         return "failed to write"
     finally:
         f.close()
-@app.route('/lux',methods=['GET'])
+@app.route('/select',methods=['GET'])
 def get_lux():
     try:
         f = open(file_path,'r')
